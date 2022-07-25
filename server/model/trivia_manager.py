0d73972220aa6f8e57c71b86c091fe0b031cfd27
Kyle Locquiao <knl5@sfu.ca>
2022-07-25 12:36:46 -0700

    Implement trivia_manager and db (update when trivia and answer class is created)

--- server/model/trivia_manager.py
+++ server/model/trivia_manager.py
@@ -1,20 +1,24 @@
 import csv
 
 class TriviaManager:
-    Trivias = []
+    trivia_list = ["Hello"]
 
     def __init__(self, filename):
-        with open('filename', newline='') as csvfile:
-            trivia_reader = csv.reader(csvfile)
+        with open(filename, newline='') as csvfile:
+            trivia_reader = csv.DictReader(csvfile)
 
             question = ""
             answer = []
             for row in trivia_reader:
                 if row["question"] != question:
                     # Create trvia question and place into Trivias array....
+                    self.trivia_list.append(row["question"])
                     question = row["question"]
                     answer = []
                 
                 # Temporary until Trivia class is made
                 answer.append({row["answer"]: eval(row["correct?"])})
 
+    def debugTriviaList(self):
+        for item in self.trivia_list:
+            print(item)
