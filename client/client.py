b9184ac4f37dea066a8325a87287a4d73a5eb173
Kyle Locquiao <knl5@sfu.ca>
2022-07-24 23:01:58 -0700

    Support JSON format instead of string

--- client/client.py
+++ client/client.py
@@ -1,7 +1,6 @@
-
-
 import socket
 import threading
+import json 
 
 HOST = "127.0.0.1"
 PORT = 12345
@@ -9,10 +8,10 @@ PORT = 12345
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 my_socket.connect((HOST, PORT))
 
-def receiver_runner(name):
+def receiver_runner():
     while True:
         try:
-            data = my_socket.recv(1024).decode('ascii')
+            data = my_socket.recv(1024).decode("utf-8")
             handle_message(data)
         except:
             print("An error has occured!")
@@ -24,7 +23,16 @@ def handle_message(data):
 
 # To be binded by front-end team members
 def send_message(message):
-    my_socket.sendall(b"%s", message)
+    data = json.dumps(message)
+    my_socket.sendall(bytes(data,encoding="utf-8"))
 
 receiver_thread = threading.thread(target=receiver_runner)
 receiver_thread.start()
+
+"""
+Token setup:
+    - Json
+    - Format: {Token: String, Data:....}
+
+
+"""
