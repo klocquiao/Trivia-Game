1f30b128d8da4103d180e62b78f80c7440072817
Kyle Locquiao <knl5@sfu.ca>
2022-08-02 16:21:27 -0700

    Copy client receiver into server.

---
+++ server/model/server.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+import json 
+
+HOST = "127.0.0.1"
+PORT = 12345
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.connect((HOST, PORT))
+
+def receiver_runner(self):
+    while True:
+        try:
+            data = my_socket.recv(1024).decode("utf-8")
+            handle_message(data)
+        except:
+            print("An error has occured!")
+            my_socket.close()
+
+def handle_message(data):
+    if data == "test":
+        tm = {"Token": "Test", "message": "Hello world!"}
+        send_message(tm)
+
+    # To be binded by front-end team members
+    def send_message(message):
+        data = json.dumps(message)
+        my_socket.sendall(bytes(data,encoding="utf-8"))
+
+    receiver_thread = threading.thread(target=receiver_runner)
+    receiver_thread.start()
+
+"""
+Token setup:
+    - Json
+    - Format: {Token: String, Data:....}
+"""
